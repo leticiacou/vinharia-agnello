852f54b7d02774537f940955b36af1a7a502c53f
Amanda <amandalourenco.2512@gmail.com>
2026-04-01 19:44:34 -0300

    docs: Criação do arquivo README com a descrição do projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+Vinheria Agnello
+
+Esse projeto se trata de um Sistema básico de cadastro e análise de vinhos artesanais da Vinheira Agnello, proposto pelo professor Lucas Sousa para um Checkpoint.
+
+Grupo:
+Amanda Oliveira Lourenço - 572572
+Giovanna Lopes Scalzonne- 572285
+Letícia Brandão Couto - 569516
+
+Engenharia de Software (1ESPJ)
--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,34 @@
 console.log('Informações de Cadastro');
 
 let nome = prompt("Digite seu nome pro cadastro:");
+alert('Nome registrado! Veja os detalhes no console');
 console.log(nome);
+
 let email = prompt("Digite seu email pro cadastro:");
+alert('Email registrado! Veja os detalhes no console');
 console.log(email);
+
 let senha = prompt("Digite sua senha pro cadastro:");
+alert('Senha registrada! Veja os detalhes no console');
 console.log(senha);
 
 alert('Cadastro realizado! Veja os detalhes no console');
  
-
-
-
-    // sobre o vinho
+// Cadastro do vinho
 console.log('Informações do vinho')
-let nomeVinho = prompt('Insira o nome do vinho: ');
-console.log(nomeVinho);
-let tipo = prompt('Insira o tipo do vinho (Branco, Tinto ou Rosé): ');
-console.log(tipo);
-let safra = prompt('Insira a safra: ');
-console.log(safra);
-let estoque = prompt('Insira a quantidade em estoque: ');
-console.log(estoque);
 
-alert('Veja os detalhes do vinho no console');
-     
+let nomeVinho = prompt('Insira o nome do vinho: ');
+alert('Nome do vinho registrado! Veja os detalhes no console');
+console.log(nomeVinho);
+
+let tipo = prompt('Insira o tipo do vinho (Branco, Tinto ou Rosé): ');
+alert('Tipo do vinho registrado! Veja os detalhes no console');
+console.log(tipo);
+
+let safra = prompt('Insira a safra(ano): ');
+alert('Safra(ano) do vinho registrado! Veja os detalhes no console');
+console.log(safra);
+
+let estoque = prompt('Insira a quantidade em estoque: ');
+alert('Estoque do vinho registrado! Veja os detalhes no console');
+console.log(estoque);
