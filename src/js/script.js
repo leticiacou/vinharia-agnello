2542b01d70b1c9d184fbc9ec5435b1db049c4d43
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 14:44:44 -0300

    fix: correção do Java Script

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,27 @@
+console.log('Informações de Cadastro');
 
 let nome = prompt("Digite seu nome pro cadastro:");
+console.log(nome);
 let email = prompt("Digite seu email pro cadastro:");
+console.log(email);
 let senha = prompt("Digite sua senha pro cadastro:");
-let cadastro = [nome, email, senha]
+console.log(senha);
+
 alert('Cadastro realizado! Veja os detalhes no console');
-console.log(cadastro);
+ 
 
 
 
-// sobre o vinho
-document.addEventListener("DOMContentLoaded", () => {
+    // sobre o vinho
+console.log('Informações do vinho')
 let nomeVinho = prompt('Insira o nome do vinho: ');
+console.log(nomeVinho);
 let tipo = prompt('Insira o tipo do vinho (Branco, Tinto ou Rosé): ');
+console.log(tipo);
 let safra = prompt('Insira a safra: ');
+console.log(safra);
 let estoque = prompt('Insira a quantidade em estoque: ');
-detalhesVinho = [nomeVinho, tipo, safra, estoque]
+console.log(estoque);
 
-alert('veja os detalhes do vinho no console');
-console.log(detalhesVinho);
-})
+alert('Veja os detalhes do vinho no console');
+     
